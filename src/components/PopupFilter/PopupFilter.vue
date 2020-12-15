725b8e99be5474fce8e0186610bcdcb7283e9377
1461433958 <30288223+1461433958@users.noreply.github.com>
2020-12-15 11:16:52 +0800

    增加组件注释和右侧弹出层筛选组件文件

---
+++ src/components/PopupFilter/PopupFilter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="popup-container">
+    <div class="popup-wrap"></div>
+    <div class="popup-main"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.popup-container {
+.popup-wrap {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 2001;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+}
+.popup-main {}
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,18 @@
-import Button from './Button/Button.vue'
-import Modal from './Modal/Modal.vue'
-import ModuleTop from './ModuleTop/ModuleTop.vue'
-import Uploader from './Uploader/Uploader.vue'
-import Test from './Test/Test.vue'
-import MapPop from './MapPop/MapPop.vue'
-import BotButton from './BotButton/BotButton.vue'
-import UnitPop from './UnitPop/UnitPop.vue'
-import FixedHeader from './FixedHeader/FixedHeader.vue'
-import FloatMenu from './FloatMenu/FloatMenu.vue'
-import InsideLoading from './InsideLoading/InsideLoading.vue'
-import NullData from './NullData/NullData.vue'
+import Button from './Button/Button.vue' //按钮
+import ModuleTop from './ModuleTop/ModuleTop.vue' //tab切换组件（带筛选、排序）
+import Uploader from './Uploader/Uploader.vue' //附件上传组件
+import Test from './Test/Test.vue'  //测试-备用
+import MapPop from './MapPop/MapPop.vue' //地图筛选弹框组件
+import BotButton from './BotButton/BotButton.vue' //底部按钮组件
+import UnitPop from './UnitPop/UnitPop.vue' //选择组织--组件
+import FixedHeader from './FixedHeader/FixedHeader.vue' //页面头部组件
+import FloatMenu from './FloatMenu/FloatMenu.vue' //悬浮球组件
+import InsideLoading from './InsideLoading/InsideLoading.vue' //动画加载中组件
+import NullData from './NullData/NullData.vue' //无数据占位组件
+import PopupFilter from './PopupFilter/PopupFilter.vue' //右侧弹出筛选组件
 
 export {
     Button,
-    Modal,
     ModuleTop,
     Uploader,
     Test,
@@ -23,5 +22,6 @@ export {
     FixedHeader,
     FloatMenu,
     InsideLoading,
-    NullData
+    NullData,
+    PopupFilter
 }
